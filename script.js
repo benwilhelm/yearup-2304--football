585601aa009936ab35ff58f64480179db6334c77
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-10 14:47:13 -0500

    exercise 2

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let teams = [
   { code: 'DAL', name: 'Dallas Cowboys', plays: 'Arlington, TX' },
   { code: 'DEN', name: 'Denver Broncos', plays: 'Denver, CO' },
   { code: 'HOU', name: 'Houston Texans', plays: 'Houston, TX' },
-  { code: 'KAN', name: 'Kansas City Chiefs', plays: 'Kansas City, MO' },
+  { code: 'KAN', name: 'Kansas City Chiefs', plays: 'Kansas City, MO' }
 ];
+const teamSelectorEl = document.getElementById('teamSelector');
 
 function playerDropDown() {
-  const teamSelectorEl = document.getElementById('teamSelector');
   for (let i = 0; i < teams.length; i++) {
     let optionEl = document.createElement('option');
     const team = teams[i];
@@ -17,3 +17,23 @@ function playerDropDown() {
 }
 
 playerDropDown();
+
+function onButtonClick() {
+  const selectedCode = teamSelectorEl.value;
+
+  let selectedTeam;
+  for (let i = 0; i < teams.length; i++) {
+    const team = teams[i];
+    if (team.code === selectedCode) {
+      selectedTeam = team;
+      break;
+    }
+  }
+
+  const message = `You selected the ${selectedTeam.name} (${selectedTeam.code}) who play in ${selectedTeam.plays}`;
+  const messageEl = document.getElementById('messagep');
+  messageEl.innerHTML = message;
+}
+
+const btn = document.getElementById('buttonInput');
+btn.onclick = onButtonClick;
